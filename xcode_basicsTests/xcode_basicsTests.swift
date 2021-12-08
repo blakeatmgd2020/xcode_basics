3c048dcb1997f97be43f6b3da21c31d7a1cc946a
Blake Wallick <bwallick@hawaii.edu>
2021-12-08 10:56:12 -1000

    Initial Commit

---
+++ xcode_basicsTests/xcode_basicsTests.swift
@@ -0,0 +1,33 @@
+//
+//  xcode_basicsTests.swift
+//  xcode_basicsTests
+//
+//  Created by Blake Wallick on 12/8/21.
+//
+
+import XCTest
+@testable import xcode_basics
+
+class xcode_basicsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
